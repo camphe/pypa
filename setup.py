a60eadac1251a1729e9f36df618472bf00046403
camphe <107952529+camphe@users.noreply.github.com>
2023-10-26 12:51:03 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,42 +5,6 @@ except:
 import os
 import sys
 from warnings import warn
-netcdf_pkgs = [('netCDF4', 'Dataset'), \
-               ('netCDF3', 'Dataset'), \
-               ('pupynere', 'NetCDFFile')]
-for pkg, reader in netcdf_pkgs:
-    try:
-        NetCDFFile = getattr(__import__(pkg, fromlist = [reader]),reader)
-        define_function = "from %s import %s as NetCDFFile" % (pkg, reader)
-        netcdfpkg = [pkg]
-        break
-    except ImportError, e:
-        warn(e.message)
-else:
-    warn("Did not find a 'true' NetCDFFile reader; 'true' NetCDF functionality will be disabled")
-    netcdfpkg = []
-    define_function = """
-class NetCDFFile(object):
-    def __init__(self, *args, **kwds):
-        raise ImportError('System has no valid netCDF reader; install netcdf4-python or pupynere')
-"""
-
-print >> file(os.path.join('src', 'pyPA', 'netcdf.py'),'wb'), """
-__all__ = ['NetCDFFile']
-__doc__ = \"\"\"
-.. _netcdf
-:mod:`netcdf` -- netcdf import point
-====================================
-
-.. module:: netcdf
-   :platform: Unix, Windows
-   :synopsis: Povides a single import point for a package.  If
-              a user has one of many netcdf interfaces, this module
-              selects it and provides it.
-.. moduleauthor:: Barron Henderson <barronh@unc.edu>
-\"\"\"
-%s
-""" % define_function
 
 def find_packages():
     import os
@@ -70,14 +34,14 @@ packages = find_packages()
 data = find_data()
 
 setup(name = 'pyPA',
-      version = '1.0rc',
+      version = '2.0',
       author = 'Barron Henderson',
       author_email = 'barronh@gmail.com',
-      maintainer = 'Barron Henderson',
-      maintainer_email = 'barronh@gmail.com',
+      maintainer = 'Cam Phelan',
+      maintainer_email = 'cam_phelan@berkeley.edu',
       packages = packages,
       package_dir = {'': 'src'},
       package_data = {'pyPA': data},
-      requires = netcdfpkg + ['numpy (>=1.2)', 'yaml', 'PseudoNetCDF'],
+      requires = ['numpy (>=1.2)', 'yaml', 'PseudoNetCDF', 'netCDF4'],
       url = 'https://code.google.com/p/pypa'
       )
